505fb6431bd5e8cb41c8a734ad1366dead563e09
adadesions <ada_dev@icloud.com>
2016-12-30 15:02:33 +0700

    First commit

---
+++ public/javascripts/NavController.js
@@ -0,0 +1,113 @@
+function showBorder(el, borderColor){
+  const value = '0.5em solid' + borderColor;
+  $(el).css({'border-bottom': value})
+  .animate({
+    borderWidth: '0.5em'
+  }, 'fast');
+}
+
+function hideBorder(el){
+  const value = 'none'
+  $(el).css({'border-bottom': value});
+}
+
+function showSubMenu(el, borderColor, speed) {
+  const ele = $(el);
+  const header = $(el+'-header');
+  header.css({
+    'padding-bottom': '1em',
+    'border-bottom-style': 'solid',
+    'border-bottom-color': borderColor
+  })
+  .animate({
+    borderWidth: '0.5em',
+  }, '1000');
+  ele.slideDown(speed);
+}
+function hideSubMenu(el, speed) {
+  const ele = $(el);
+  const header = $(el+'-header');
+  header.css({
+    'padding-bottom': '0',
+    'border-bottom': 'none'
+  });
+  ele.slideUp(speed);
+}
+
+$(function() {
+  const logo = $('#logo');
+  const home = $('#nav-home');
+  const accomodation = $('#nav-accomodation');
+  const services = $('#nav-services');
+  const facilities = $('#nav-facilities');
+  const aboutus = $('#nav-aboutus');
+
+// HOME
+  home.mouseenter(function () {
+    showBorder(this, '#F9A113');
+  });
+
+  home.mouseleave(function () {
+    hideBorder(this);
+  });
+// END HOME
+
+// ACCOMODATION
+  accomodation.mouseenter(function(){
+    showBorder(this, '#7F1118');
+    showSubMenu('#sub-accomodation', '#7F1118', 500);
+  });
+  accomodation.mouseleave(function(){
+    $(this).css({'border-bottom': 'none'});
+    hideSubMenu('#sub-accomodation', 'fast');
+    $('#submenu-accomodation-picture').fadeOut('fast');
+    $('#submenu-right-accomodation').css({
+      display: 'none',
+      left: 0
+    });
+  });
+
+  $('#sub-accomodation').mouseover(function () {
+    $('#submenu-right-accomodation')
+    .css({display: 'block'})
+    .animate({
+      left: '2.5em'
+    },800);
+  });
+// END ACCOMODATION
+
+// SERVICES
+  services.mouseenter(function () {
+    showBorder(this, '#5101C0');
+  });
+
+  services.mouseleave(function () {
+    hideBorder(this);
+  });
+// END SERVICES
+
+// FACILITIES
+  facilities.mouseenter(function () {
+    showBorder(this, '#910283');
+    showSubMenu('#sub-facilities', '#910283', 500);
+  });
+
+  facilities.mouseleave(function () {
+    hideBorder(this);
+    hideSubMenu('#sub-facilities', 'fast');
+  });
+// END FACILITIES
+
+// ABOUT US
+  aboutus.mouseenter(function () {
+    showBorder(this, '#411ED3');
+    showSubMenu('#sub-aboutus', '#411ED3', 500);
+  });
+
+  aboutus.mouseleave(function () {
+    hideBorder(this);
+    hideSubMenu('#sub-aboutus', 'fast');
+  });
+// END ABOUT US
+
+});

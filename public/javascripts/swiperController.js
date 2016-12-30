a3c71e8ca289770c135ab978c94df77ae109c85d
adadesions <ada_dev@icloud.com>
2016-12-30 16:40:53 +0700

    add slide to greeting section

---
+++ public/javascripts/swiperController.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+   //initialize swiper when document ready
+   var mySwiper = new Swiper ('.swiper-container', {
+     // Optional parameters
+     direction: 'horizontal',
+     speed: 1000,
+     autoplay: 2000,
+     effect: 'cube',
+     parallax: true,
+     grabCursor: true,
+     touchEventsTarget: '.swiper-container',
+     lazyLoading: true,
+     loop: true
+   })
+  });

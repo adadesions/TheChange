78449737bf7427fb2b8a594b026cb9f7de3ee85c
adadesions <ada_dev@icloud.com>
2017-01-01 02:10:03 +0700

    Complete responsive for mobile

--- public/javascripts/swiperController.js
+++ public/javascripts/swiperController.js
@@ -1,17 +1,38 @@
 $(document).ready(function () {
-   //initialize swiper when document ready
-   var greetingSwiper = new Swiper ('#greeting-section-slide', {
-     // Optional parameters
-     direction: 'horizontal',
-     speed: 1000,
-     autoplay: 2000,
-     effect: 'cube',
-     parallax: true,
-     grabCursor: true,
-     touchEventsTarget: '.swiper-container',
-     lazyLoading: true,
-     loop: true
-   });
+  var width = $(window).width();
+  var height = $(window).height();
+
+   if( width >= 300)
+   {
+     var greetingSwiper = new Swiper ('#greeting-section-slide', {
+       // Optional parameters
+       direction: 'horizontal',
+       speed: 1000,
+       autoplay: 2000,
+       effect: 'fade',
+       parallax: true,
+       grabCursor: true,
+       touchEventsTarget: '.swiper-container',
+       lazyLoading: true,
+       loop: true
+     });
+   }
+   else
+   {
+     var greetingSwiper = new Swiper ('#greeting-section-slide', {
+       // Optional parameters
+       direction: 'horizontal',
+       speed: 1000,
+       autoplay: 2000,
+       effect: 'cube',
+       parallax: true,
+       grabCursor: true,
+       touchEventsTarget: '.swiper-container',
+       lazyLoading: true,
+       loop: true
+     });
+   }
+
 
    var roomSwiper = new Swiper('#room-section', {
      parallax: true,
